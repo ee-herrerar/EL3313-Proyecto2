8a5fb668a8304fe8fd64ee5b702b9ed9fc3b7c1c
Juan90ww <j.obando.3@estudiantec.cr>
2026-09-07 11:19:43 -0600

    Add LEDStatus module

--- modulos/src/leds.sv
+++ modulos/src/leds.sv
@@ -1,1 +1,21 @@
+module LEDStatus (
+    input  logic       GameActivo,
+    input  logic       GameOver,
+    input  logic       UltimoAcierto,
+    input  logic       UltimoFallo,
 
+    output logic [3:0] LED
+);
+
+    always_comb begin
+
+        LED = 4'b0000;
+
+        LED[0] = GameActivo;
+        LED[1] = GameOver;
+        LED[2] = UltimoAcierto;
+        LED[3] = UltimoFallo;
+
+    end
+
+endmodule
